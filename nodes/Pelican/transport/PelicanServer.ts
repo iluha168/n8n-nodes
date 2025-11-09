9b08f5f425a843a999d027b50b85f03306603ff0
iluha168 <illya.chernyshov888@gmail.com>
2025-11-09 13:04:30 +0700

    fix: handle "jwt error"

--- nodes/Pelican/transport/PelicanServer.ts
+++ nodes/Pelican/transport/PelicanServer.ts
@@ -86,6 +86,10 @@ type EventClientbound =
 			args: never;
 	  }
 	| {
+			event: 'jwt error';
+			args: [message: string];
+	  }
+	| {
 			event: 'console output' | 'install output';
 			args: [console: string];
 	  }
@@ -128,6 +132,7 @@ export class PelicanServer extends EventEmitter<{
 					break;
 				case 'token expiring':
 				case 'token expired':
+				case 'jwt error':
 					this.send('auth', await getJWT());
 					break;
 				case 'status':
